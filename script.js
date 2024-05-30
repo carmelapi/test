7dcf6306c455acadd2184bba1a9a2ffdf19337e9
Carmela Pippa <carmelapippa@gmail.com>
2024-05-30 09:00:26 +0200

    create test

---
+++ script.js
@@ -0,0 +1,16 @@
+//gsap.to()... infinity and beyond!
+gsap.registerPlugin(ScrollTrigger);
+const containerWidth = document.querySelector(".eye-container").scrollWidth;
+const eyeWidth = document.querySelector(".eyes").scrollWidth;
+
+console.log(containerWidth);
+//To learn more about using GreenSock's bonus plugins for free on CodePen visit: https://gsap.com/trial
+gsap.to(".eye-container", {
+  x: -(containerWidth - eyeWidth),
+  scrollTrigger: {
+    trigger: ".pinned-container",
+    scrub: 0,
+    start: "top top",
+    end: "bottom bottom",
+  },
+});
